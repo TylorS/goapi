cd58546ccb322e5cb2dfccc89e7f2e521876b6e4
Tylor Steinberger <tlsteinberger167@gmail.com>
2015-05-23 12:59:24 +0000

    Update blueprints to have default middleware

--- blueprint.go
+++ blueprint.go
@@ -1,5 +1,7 @@
 package goapi
 
+import "github.com/tylors/goapi/middleware"
+
 type BluePrint interface {
 	RegisterRouter(*App, string)
 	Initialize()
@@ -12,4 +14,8 @@ type Blueprint struct {
 
 func (b *Blueprint) RegisterRouter(app *App, prefix string) {
 	b.Router = app.Router.Subrouter(prefix)
+}
+
+func (b *Blueprint) Initialize() {
+	b.Use(middleware.Logger, middleware.Recovery)
 }
